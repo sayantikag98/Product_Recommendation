fbeb0868c06a3bc7d60d6a4455620ab7134eea73
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2023-02-24 12:09:52 +0530

    Added optimization for second condition

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import {productDatabase} from "./data.js";
 const userInput = document.getElementById("user-form");
-let userProduct = "", userRating = 0, userProductCategory = "", recommendedProduct = "";
+let userProduct = "", userRating = 0, userProductCategory = "", recommendedProduct = "", condition = "";
 
 /**
  * sorts the array based on the custom order of decreasing rating and then increasing or decreasing order of prices based on isPriceIncreasing parameter.
@@ -31,21 +31,34 @@ const sortArray = (isPriceIncreasing, productsFiltered) => {
 
 const onFormSubmit = (event) => {
     event.preventDefault();
+    // getting form entries
     const formValue = new FormData(event.target);
     const formEntries = Object.fromEntries(formValue);
     userProduct = formEntries.product;
     userRating = parseFloat(formEntries.rating);
     userProductCategory = productDatabase.filter(item => item.name === userProduct)[0].category;
-    recommendedProduct = "No Product Found";
+    if(userRating>4.5 || (userRating>= 4 && userRating<=4.5 && condition!=="second") || userRating<4){
+        recommendedProduct = "No Product Found";
+    }
 
     if(userRating>4.5){
+        if(condition !== "first") condition = "first";
+        // filtering out all the products which have rating above 4.3 and not the product given by the user
         const productsFiltered = productDatabase.filter(item => item.rating>4.3 && item.name !== userProduct);
         recommendForHigherRating(productsFiltered);
     }else if(userRating>=4 && userRating<=4.5){
-        const productsFiltered = productDatabase.filter(item => item.rating>4.5);
-        recommendForHigherRating(productsFiltered); 
+        // condition used to prevent repeatedly calling the recommendation criteria
+        if(condition !== "second"){
+            condition = "second";
+            //filtering out products which have rating above 4.5
+            const productsFiltered = productDatabase.filter(item => item.rating>4.5);
+            recommendForHigherRating(productsFiltered);
+        }   
     }else{
+        if(condition !== "third") condition = "third";
+        // filtering out products which have category different than the user given product category
         const productsFiltered = productDatabase.filter(item => item.category !== userProductCategory);
+        // sorting to get the highest rated product with lowest price in case of a match
         sortArray(true, productsFiltered);
         recommendedProduct = productsFiltered[0].name;
     }
@@ -63,6 +76,7 @@ const recommendForHigherRating = (productsFiltered) => {
     sortArray(false, productsFiltered);
     let firstRating = productsFiltered[0].rating, subArr = [];
     const newFilteredArray = []; 
+    // to get all the products having two or more same ratings
     productsFiltered.forEach(item => {
         if(item.rating === firstRating){
             subArr.push(item);
@@ -77,6 +91,7 @@ const recommendForHigherRating = (productsFiltered) => {
     if(subArr.length>=2)
         newFilteredArray.push(subArr);
 
+        // getting the products having the same category as the product given by the user 
     let tempName = "";
     for(let i = 0; i<newFilteredArray.length; i++){
         let count = 0;
@@ -93,6 +108,8 @@ const recommendForHigherRating = (productsFiltered) => {
             break;
         } 
     }
+
+    // in case of a no match of the same catgory using highest price as the criteria
 
     if(recommendedProduct === "No Product Found"){
         let maxPrice = -1;
